a10deb75be4daee020aec75b90599252da0d4509
Vikram <vikram@nandi.net>
2022-02-14 14:38:14 -0800

    lol

---
+++ test.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+
+capture = cv.VideoCapture('Videos\Test.mp4')
+
+#While loop to show frame by frame
+while True:
+    #reads frame by frame
+    isTrue, frame = capture.read()
+    #shows frame by frame
+    cv.imshow('Video', frame)
+
+    #If waitKey delay is 20 miliseconds and d key is pressed, video ends
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+#realeases the path from capture
+capture.release()
+#destroys all the windows
+cv.destroyAllWindows()
+cv.waitKey(0)
